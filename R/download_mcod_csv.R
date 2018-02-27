6d44d33555d964977a5d01e564f630ecbea05c20
Mathew Kiang <mathew.kiang@gmail.com>
2018-02-26 19:57:54 -0500

    Update download mcod to also have an option for territories

--- R/download_mcod_csv.R
+++ R/download_mcod_csv.R
@@ -5,14 +5,16 @@
 #' specified folder in the CSV format.
 #'
 #' @param year year to download (as integer)
-#' @param download_dir file path to save dowhlo data
+#' @param download_dir file path to save download data
+#' @param territories download US territories
 #'
 #' @return none
 #' @export
 #' @importFrom utils download.file
 #' @source http://www.nber.org/data/vital-statistics-mortality-data-multiple-cause-of-death.html
 
-download_mcod_csv <- function(year, download_dir = './raw_data') {
+download_mcod_csv <- function(year, download_dir = './raw_data',
+                              territories = FALSE) {
     ## Downloads the raw MCOD data (as csv) for specified year
     ##
     ## Source: print(paste0('http://www.nber.org/data/vital-statistics',
@@ -20,7 +22,7 @@ download_mcod_csv <- function(year, download_dir = './raw_data') {
 
     ## Create URL
     base_url  <- "http://www.nber.org/mortality"
-    file_name <- sprintf('mort%s.csv', year)
+    file_name <- sprintf('mort%s%s.csv', ifelse(territories, "terr", ""), year)
     file_url  <- sprintf("%s/%s/%s.zip", base_url, year, file_name)
     dest_file <- sprintf('%s/%s.zip', download_dir, file_name)
 
